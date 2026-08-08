7179b87921b5a8fef6605c89ad4cef2fc4e036bd
Brent Cordis <994259+bcordis@users.noreply.github.com>
2026-08-07 23:02:11 -0500

    chore: bump version to 1.1.9

--- src/LibraryVersion.php
+++ src/LibraryVersion.php
@@ -57,7 +57,7 @@ class LibraryVersion
      * @var  string
      * @since  1.0.0
      */
-    public const VERSION = '1.1.8';
+    public const VERSION = '1.1.9';
 
     /**
      * Check if the library extension is installed in Joomla.
