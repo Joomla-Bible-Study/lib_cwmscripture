2e81c665c1cbaf651efde75ebc3ad4e9389b1542
Brent Cordis <994259+bcordis@users.noreply.github.com>
2026-08-13 13:20:12 -0500

    chore: bump version to 1.1.14

--- src/LibraryVersion.php
+++ src/LibraryVersion.php
@@ -57,7 +57,7 @@ class LibraryVersion
      * @var  string
      * @since  1.0.0
      */
-    public const VERSION = '1.1.13';
+    public const VERSION = '1.1.14';
 
     /**
      * Check if the library extension is installed in Joomla.
